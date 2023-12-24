e79d006340951a925188dcd64ab18398a6f11163
mr-sazzad <main.sazzad@gmail.com>
2023-12-24 13:14:06 +0600

    some modified

--- src/app/users/user.validation.ts
+++ src/app/users/user.validation.ts
@@ -4,7 +4,7 @@ export const validateCreateUser = z.object({
   body: z.object({
     name: z.string({ required_error: "name is required" }),
     email: z.string({ required_error: "email is required" }),
-    passWord: z.string({ required_error: "passWord is required" }),
+    password: z.string({ required_error: "passWord is required" }),
     bio: z.string().optional(),
     role: z.enum(["user", "admin"], { required_error: "role is required" }),
     phone: z.string().optional(),
